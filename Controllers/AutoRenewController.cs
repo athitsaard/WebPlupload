a084f96b63693a9b068f72eb96654628922bd80f
Athit Sa-ard <athit.saard@home.dga.or.th>
2025-07-05 21:24:24 +0700

    edit autorenew

--- Controllers/AutoRenewController.cs
+++ Controllers/AutoRenewController.cs
@@ -21,9 +21,59 @@ namespace WebPlupload.Controllers
         [Route("Index")]
         public IActionResult Index()
         {
+            //var status1 = false;
+            //var status2 = false;
+            //var status3 = false;
+
+            //for (int i = 0; i < 1; i++)
+            //{
+            //    Console.WriteLine("i = " + i);
+
+            //    var method1 = true;
+            //    status1 = method1;
+
+
+            //    if (status1 == true)
+            //    {
+
+            //        var method2 = true;
+            //        status2 = method2;
+
+            //    }
+            //    else 
+            //    {
+            //        Console.WriteLine("method1 fail");
+
+            //    }
 
 
 
+
+            //    if (status1 && status2 == true)
+            //    {
+            //        var method3 = false;
+            //        status3 = method3;
+            //    }
+            //    else if(status1 == true && status2 == false)
+            //    {
+            //        Console.WriteLine("method2 fail");
+            //    }
+
+
+
+
+            //    if (status1 && status2 && status3 == true)
+            //    {
+            //        var method4 = false;
+
+            //    }
+            //    else if (status1 == true && status2 == true && status3 == false)
+            //    {
+            //        Console.WriteLine("method3 fail");
+            //    }
+
+
+            //}
 
 
 
